a260224d314f0e93e658628f3894b377a25b5857
shampi75 <shrikanth.hampiholi@gmail.com>
2025-03-20 15:08:31 +0530

    Update SimpleCalculatorTest.java

--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -7,6 +7,10 @@ public class SimpleCalculatorTest {
         var Calculator  = new SimpleCalculator();
         assertEquals(4, Calculator.add(2,2));
     }
-
+@Test
+    public void threePlusfourEqSeven(){
+        var Calculator  = new SimpleCalculator();
+        assertEquals(7, Calculator.add(3,4));
+    }
 
 }
